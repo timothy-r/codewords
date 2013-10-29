23e26d9091d24c6abecf3303103d6012c158ec48
Tim Rodger <tim.rodger@gmail.com>
2013-10-29 06:58:31 +0000

    Adds tests for setting and getting Cells in a Board

--- Tests/BoardTest.php
+++ Tests/BoardTest.php
@@ -2,12 +2,24 @@
 require_once(__DIR__ . '/BaseTest.php');
 
 use Codewords\Board;
+use Codewords\Cell;
 
 class BoardTest extends BaseTest
 {
-    public function testBoard()
+    public function testCanAddCellToBoard()
     {
         $board = new Board;
+        $cell = new Cell(1);
+        $board->addCell($cell, 0, 0);
+    }
+
+    public function testCanGetCellToBoard()
+    {
+        $board = new Board;
+        $cell = new Cell(1);
+        $board->addCell($cell, 0, 0);
+        $result = $board->getCell(0,0);
+        $this->assertsame($cell, $result);
     }
 }
 
--- lib/Codewords/Board.php
+++ lib/Codewords/Board.php
@@ -1,5 +1,7 @@
 <?php namespace Codewords;
 
+use Codewords\Cell;
+
 /**
 * Represents the grid of 13 x 13 Cells
 * Contains the Cells
@@ -7,5 +9,32 @@
 */
 class Board
 {
+    /**
+    * @var array
+    */
+    protected $rows;
 
+    public function __construct()
+    {
+        $this->rows = [];
+        for ($y = 0; $y < 13; $y++) {
+            $row = [];
+            for($x = 0; $x < 13; $x++) {
+                $row[$x] = null;
+            }
+            $this->rows []= $row;
+        }
+    }
+
+    public function addCell(Cell $cell, $x, $y)
+    {
+        $row = &$this->rows[$y];
+        $row[$x] = $cell;
+    }
+
+    public function getCell($x, $y)
+    {
+        $row = $this->rows[$y];
+        return $row[$x]; 
+    }
 }
