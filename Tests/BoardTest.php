342044c0ad9a3aebfa0a8bd93a46795baf01b257
Tim Rodger <tim.rodger@gmail.com>
2013-10-29 06:47:41 +0000

    Adds Board class

---
+++ Tests/BoardTest.php
@@ -0,0 +1,13 @@
+<?php
+require_once(__DIR__ . '/BaseTest.php');
+
+use Codewords\Board;
+
+class BoardTest extends BaseTest
+{
+    public function testBoard()
+    {
+        $board = new Board;
+    }
+}
+
---
+++ lib/Codewords/Board.php
@@ -0,0 +1,11 @@
+<?php namespace Codewords;
+
+/**
+* Represents the grid of 13 x 13 Cells
+* Contains the Cells
+* Allows access to rows, columns and cells
+*/
+class Board
+{
+
+}
