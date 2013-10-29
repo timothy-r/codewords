86842f0ad17be907a2690686fe10d49774e617a9
Tim Rodger <tim.rodger@gmail.com>
2013-10-29 06:43:08 +0000

    Adds a Game class

---
+++ Tests/GameTest.php
@@ -0,0 +1,12 @@
+<?php
+require_once(__DIR__ . '/BaseTest.php');
+
+use Codewords\Game;
+
+class GameTest extends BaseTest
+{
+    public function testSetCellCharacter()
+    {
+
+    }
+}
---
+++ lib/Codewords/Game.php
@@ -0,0 +1,30 @@
+<?php namespace Codewords;
+
+/**
+* The entry point for Codewords applications
+*/
+class Game
+{
+    public function __construct()
+    {
+
+    }
+
+    public function setCellCharacter($number, $character)
+    {
+
+    }
+
+    public function getBoard()
+    {
+    }
+
+    public function getCells()
+    {
+    }
+
+    public function clearBoard()
+    {
+
+    }
+}
