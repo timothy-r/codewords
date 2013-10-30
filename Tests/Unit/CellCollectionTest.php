c9a54f9ca816c956acd334e90bfd396eae36a36f
Tim Rodger <tim.rodger@gmail.com>
2013-10-30 23:41:24 +0000

    More CellCollection tests

--- Tests/Unit/CellCollectionTest.php
+++ Tests/Unit/CellCollectionTest.php
@@ -43,4 +43,13 @@ class CellCollectionTest extends BaseTest
         $cell_collection = new CellCollection;
         $cell = $cell_collection->cell($number);
     }
+
+    public function testGetCellReturnsAReferenceToACell()
+    {
+        $cell_collection = new CellCollection;
+        $cell_1 = $cell_collection->cell(1);
+        $cell_1->setCharacter('A');
+        $cell_2 = $cell_collection->cell(1);
+        $this->assertSame('A', $cell_2->getCharacter());
+    }
 }
