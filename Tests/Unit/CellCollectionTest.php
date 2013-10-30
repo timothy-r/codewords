1bf431529533c23e7895747e286109991bd9e8d6
Tim Rodger <tim.rodger@gmail.com>
2013-10-30 23:30:17 +0000

    Implements CellCollection::cell()

--- Tests/Unit/CellCollectionTest.php
+++ Tests/Unit/CellCollectionTest.php
@@ -9,10 +9,19 @@ use Codewords\CellCollection;
 */
 class CellCollectionTest extends BaseTest
 {
-    public function testGetCell()
+    public function testGetCellReturnsACell()
     {
         $cell_collection = new CellCollection;
         $cell = $cell_collection->cell(1);
         $this->assertInstanceOf('Codewords\Cell', $cell);
+    }
+
+    public function testCallsToGetCellReturnsSameInstance()
+    {
+        $cell_collection = new CellCollection;
+        $cell_1 = $cell_collection->cell(1);
+        $cell_2 = $cell_collection->cell(1);
+        // assertSame tests that the two objects reference the same instance
+        $this->assertSame($cell_1, $cell_2);
     }
 }
--- lib/Codewords/CellCollection.php
+++ lib/Codewords/CellCollection.php
@@ -4,12 +4,17 @@ use Codewords\Cell;
 
 class CellCollection
 {
+    protected $cells = [];
+
     /**
     *
     * @return Codewords\Cell
     */
     public function cell($number)
     {
-
+        if (!isset($this->cells[$number])){
+            $this->cells[$number] = new Cell($number);
+        }
+        return $this->cells[$number];
     }
 }
