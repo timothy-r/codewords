35ad455b142484d4b3aaf2c14027958e43e49e2d
Tim Rodger <tim.rodger@gmail.com>
2013-11-05 21:28:09 +0000

    Implements at() and contains() for Word

--- Tests/Unit/WordTest.php
+++ Tests/Unit/WordTest.php
@@ -1,15 +1,30 @@
 <?php
-
 require_once(__DIR__ . '/BaseTest.php');
 
 use Codewords\Word;
+use Codewords\Cell;
 
 /**
 * @group unit
 */
 class WordTest extends BaseTest
 {
-    public function testWordHasLetters()
+    public function testCanAccessCellsInWord()
     {
+        $cells = [new Cell(1), new Cell(2)];
+        $word = new Word($cells);
+        $cell = $word->at(0);
+        $this->assertTrue($cell->matches($cells[0]));
+        $cell = $word->at(1);
+        $this->assertTrue($cell->matches($cells[1]));
+    }
+
+    public function testWordContainsCells()
+    {
+        $cells = [new Cell(1), new Cell(2)];
+        $word = new Word($cells);
+        $this->assertTrue($word->contains($cells[0]));
+        $this->assertTrue($word->contains($cells[1]));
+        $this->assertFalse($word->contains(new Cell(3)));
     }
 }
--- lib/Codewords/Word.php
+++ lib/Codewords/Word.php
@@ -1,18 +1,36 @@
 <?php namespace Codewords;
 
+use Codewords\Cell;
+
+/**
+* Represents a Word, set of Cells
+*/
 class Word
 {
+    /**
+    * @var array
+    */
     protected $word;
 
-    public function __construct($word)
+    public function __construct(array $word)
     {
         $this->word = $word;    
     }
 
-    public function letter($index)
+    public function at($index)
     {
-        if ($index >= 0 && $index <strlen($this->word)){
+        if ($index >= 0 && $index < count($this->word)){
+            return $this->word[$index];
         }
     }
 
+    public function contains(Cell $other)
+    {
+        foreach($this->word as $cell){
+            if ($other->matches($cell)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
