d0caa565b9b6a9d4d4cb89b74784bd8a02a083ea
Tim Rodger <tim.rodger@gmail.com>
2013-11-15 20:51:52 +0000

    Adds detecting which Cells are followed by a single other Cell

--- lib/Codewords/Solver/FindQ.php
+++ lib/Codewords/Solver/FindQ.php
@@ -20,9 +20,19 @@ class FindQ
     public function solve()
     {
         $results = [];
+
         // find Cells followed by only 1 other Cell
+        $following_letter_count = $this->game->getStatsRepository()->getStat('FollowingLetter');
+        $following_letters = $following_letter_count->generate($this->game);
+        foreach ($following_letters as $number => $value){
+            if (count($value) === 1){
+                $results []= $number;
+            }
+        }
 
         // check which Cells appear at the ends of Words - they are not Qs
+        
+        var_dump($results);
 
         return $results;
     }
