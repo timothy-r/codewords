5acc2c5c7fa82f865779e5f2fecb2cc80824a856
Tim Rodger <tim.rodger@gmail.com>
2013-11-17 22:44:22 +0000

    Adds FindI solver class

---
+++ Tests/Integration/Solver/FindITest.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once(__DIR__ . '/../IntegrationTest.php');
+require_once(__DIR__. '/../FixtureTrait.php');
+
+use Codewords\Solver\FindI;
+
+class FindITest extends IntegrationTest
+{
+    use FixtureTrait;
+
+    public function getValidGameData()
+    {
+        return [
+            ['data-1.csv',['1','2','3','4','5','6','7','8','9','10','11','12','13','15','16','17','19','20','21','22','23','24','25','26']],
+            ['data-3.csv',['1','3','4','5','7','8','9','10','11','12','13','14','15','16','17','19','20','21','23','24','25','26']],
+        ];
+    }
+    
+    /**
+    * @dataProvider getValidGameData
+    */
+    public function testSolveReturnsArrayOfCells($fixture, $expected)
+    {
+        $this->givenAFileDictionary();
+        $this->givenAGame($fixture);
+
+        $solver = new FindI($this->game);
+        $results = $solver->solve();
+        
+        $this->assertTrue(is_array($results), "Expected solve() to return an array");
+        $this->assertSame(count($expected), count($results), "Expected solve() to return ".count($expected)." results");
+        foreach($results as $cell) {
+            $this->assertInstanceOf('Codewords\Cell', $cell);
+            $this->assertTrue(in_array($cell->getNumber(), $expected), "Didn't expect " . $cell->getNumber() . " to be a possible I");
+        }
+    }
+}
---
+++ lib/Codewords/Solver/FindI.php
@@ -0,0 +1,56 @@
+<?php namespace Codewords\Solver;
+
+use Codewords\Game;
+use Codewords\Cell;
+
+/**
+* Finds Cells that may be an I letter
+*/
+class FindI
+{
+    protected $game;
+
+    public function __construct(Game $game)
+    {
+        $this->game = $game;
+    }
+
+    /**
+    * @return array of Cell objects
+    */
+    public function solve()
+    {
+        $results = [];
+        
+        $cells = $this->game->getCells();
+        for ($c = 1; $c < 27; $c++) {
+            $cell = $cells->at($c);
+            if ($this->isOrCanBeI($cell)){
+                $results[]= $cell;
+            }
+        }
+        return $results;
+    }
+
+    protected function isOrCanBeI(Cell $cell)
+    {
+        $character = $cell->getCharacter();
+
+        if ($character === 'I'){
+            return true;
+        }
+
+        if ($character !== null) {
+            return false;
+        }
+       
+        // test if Cell appears as a double
+        $stats = $this->game->getStatsRepository()->getStat('DoubleLetter');
+        $double_letters = $stats->generate($this->game);
+        if ($double_letters[$cell->getNumber()] > 0){
+            return false;
+        }
+
+        return true;
+    }
+}
