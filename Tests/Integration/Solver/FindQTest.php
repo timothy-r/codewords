aa9dfe09fe2e13b9b045b7e2258cff38d9a0f1f3
Tim Rodger <tim.rodger@gmail.com>
2013-11-17 18:28:52 +0000

    Implements initial FindQ solver class

--- Tests/Integration/Solver/FindQTest.php
+++ Tests/Integration/Solver/FindQTest.php
@@ -12,14 +12,14 @@ class FindQTest extends IntegrationTest
     public function getValidGameData()
     {
         return [
-            ['data-1.csv']
+            ['data-1.csv', ['3']]
         ];
     }
     
     /**
     * @dataProvider getValidGameData
     */
-    public function testSolveReturnsArrayOfCells($fixture)
+    public function testSolveReturnsArrayOfCells($fixture, $expected)
     {
         $this->givenAFileDictionary();
         $this->givenAGame($fixture);
@@ -27,12 +27,11 @@ class FindQTest extends IntegrationTest
         $solver = new FindQ($this->game);
         $results = $solver->solve();
 
-        var_dump($results);
-
         $this->assertTrue(is_array($results), "Expected solve() to return an array");
-        $this->assertTrue(count($results) > 0, "Expected solve() to return 1+ results");
-        foreach($results as $result) {
-            $this->assertInstanceOf('Codewords\Cell', $result);
+        $this->assertSame(count($expected), count($results), "Expected solve() to return ".count($expected)." results");
+        foreach($results as $cell) {
+            $this->assertInstanceOf('Codewords\Cell', $cell);
+            $this->assertTrue(in_array($cell->getNumber(), $expected), "Didn't expect " . $cell->getNumber() . " to be a possible Q");
         }
     }
 }
--- lib/Codewords/Solver/FindQ.php
+++ lib/Codewords/Solver/FindQ.php
@@ -1,6 +1,7 @@
 <?php namespace Codewords\Solver;
 
 use Codewords\Game;
+use Codewords\Cell;
 
 /**
 * Finds Cells that may be a Q letter
@@ -28,7 +29,7 @@ class FindQ
             if (count($value) === 1){
                 // test if value can be a U or is a U
                 $cell = current($value);
-                if ($cell->getCharacter() === 'U' || $cell->getCharacter() === null){
+                if ($this->isOrCanBeU($cell)){
                     $result = $this->game->getCells()->at($number);
                     $results []= $result;
                 }
@@ -38,5 +39,34 @@ class FindQ
         // check which Cells appear at the ends of Words - they are not Qs
 
         return $results;
+    }
+
+    protected function isOrCanBeU(Cell $cell)
+    {
+        $character = $cell->getCharacter();
+
+        if ($character === 'U'){
+            return true;
+        }
+
+        if ($character !== null) {
+            return false;
+        }
+       
+        // test if Cell appears as a double
+        $stats = $this->game->getStatsRepository()->getStat('DoubleLetter');
+        $double_letters = $stats->generate($this->game);
+        if ($double_letters[$cell->getNumber()] > 0){
+            return false;
+        }
+
+        // test if Cell appears last in any word
+        $stats = $this->game->getStatsRepository()->getStat('LastLetter');
+        $last_letters = $stats->generate($this->game);
+        if ($last_letters[$cell->getNumber()] !== 0){
+            return false;
+        }
+
+        return true;
     }
 }
