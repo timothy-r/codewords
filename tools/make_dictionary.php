be28781aa625a1244999b9ea4886f763bf7893eb
Tim Rodger <tim.rodger@gmail.com>
2013-11-18 20:54:47 +0000

    First version of dict creation tool

---
+++ tools/make_dictionary.php
@@ -0,0 +1,67 @@
+<?php
+
+function read_dict($file)
+{
+    $dict = [];
+
+    if (file_exists($file)){
+        $dict = require_once($file);
+    }
+
+    // convert each item into a list
+    foreach($dict as $key => &$item) {
+        $item = explode("\n", $item);
+    }
+    return $dict;
+}
+
+function write_dict(array $dict, $file)
+{
+    // convert each item into a list
+    foreach($dict as $key => &$item) {
+        sort($item);
+        $item = implode("\n", $item);
+    }
+
+    // write $dict back to $data file
+    $str = var_export($dict, $return = true);
+
+    file_put_contents($file, '<' . "?php\nreturn $str;\n");
+}
+
+// $in is a file with a list of words, one per line
+// $dict is the file to write the data to (it may exist already)
+$in = $argv[1];
+$data = $argv[2];
+
+$dict = read_dict($data);
+
+// read $in into a flat array
+$lines = file($in);
+
+$count = 0;
+$min_length = 3;
+
+// add each item in $new to $data;
+foreach($lines as $word){
+    $word = strtolower(trim($word));
+    $len = strlen($word);
+    if ($len < $min_length){
+        continue;
+    }
+
+    // add to $dict
+    if (!isset($dict[$len])){
+        $dict[$len] = [];
+    }
+    if (!in_array($word, $dict[$len])){
+        $dict[$len] []= $word;
+    }
+    $count++;
+    if ($count % 100 == 0){
+        print "$count\n";
+    }
+}
+
+write_dict($dict, $data);
+
