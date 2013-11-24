b929e87747851a74adfb4c32cb517a7af1b2360c
Tim Rodger <tim.rodger@gmail.com>
2013-11-24 20:24:08 +0000

    Implements reducing wild card chars in WordPattern

--- Tests/Unit/WordPatternTest.php
+++ Tests/Unit/WordPatternTest.php
@@ -2,6 +2,7 @@
 require_once(__DIR__ . '/BaseTest.php');
 
 use Codewords\Board\Cell;
+use Codewords\Board\CellCollection;
 use Codewords\Board\Word;
 use Codewords\Solver\WordPattern;
 
@@ -10,27 +11,33 @@ use Codewords\Solver\WordPattern;
 */
 class WordPatternTest extends BaseTest
 {
+    protected $cells;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->cells = new CellCollection;
+    }
+
     public function getLetters()
     {
         return [
-            ['J', [[0, null], [1, 'O'], [2, null], [3, 'E']], '^(J)(O).(E)$'],
-            ['W', [[0, null], [1, 'A'], [2, null], [4, null]], '^(W)(A)..$'],
-            ['D', [[0, null], [1, 'R'], [2, null], [3, null], [4, 'T'], [5, 'Y']], '^(D)(R)..(T)(Y)$'],
-            ['W', [[0, null], [1, null], [2, 'L'], [2, 'L']], '^(W).(L)\2$'],
-            ['W', [[0, null], [1, 'E'], [2, null], [1, 'E']], '^(W)(E).\2$'],
-            ['D', [[0, null], [1, 'R'], [2, null], [1, 'R'], [4, 'T'], [5, 'Y']], '^(D)(R).\2(T)(Y)$'],
-            ['D', [[0, null], [1, 'R'], [2, null], [3, 'E'], [4, 'T'], [4, 'T']], '^(D)(R).(E)(T)\4$'],
+            ['J', [[0, null], [1, 'O'], [2, null], [2, null]], '^JO([^J|O])\1$', 0],
+            ['W', [[0, null], [1, 'A'], [2, null], [4, null]], '^WA([^A|W])([^A|W])$', 0],
+            ['W', [[0, null], [1, null], [2, 'L'], [1, null]], '^W([^L|W])L\1$', 0],
+            ['D', [[0, null], [1, 'R'], [2, null], [2, null], [4, 'T'], [5, 'Y']], '^DR([^D|R|T|Y])\1TY$', 0],
+            ['D', [[0, null], [1, 'R'], [2, null], [3, 'E'], [4, 'T'], [2, null]], '^DR([^D|E|R|T])ET\1$', 0],
         ];
     }
 
     /**
     * @dataProvider getLetters
     */
-    public function testMakeForLetter($letter, $cell_data, $expected)
+    public function testMakeForLetter($letter, $cell_data, $expected, $position)
     {
         $word = $this->makeWord($cell_data);
-        $word_pattern = new WordPattern;
-        $cell = $word->at(0);
+        $word_pattern = new WordPattern($this->cells);
+        $cell = $word->at($position);
         $actual = $word_pattern->make($letter, $cell, $word); 
         $this->assertSame($expected, $actual);
     }
@@ -40,15 +47,15 @@ class WordPatternTest extends BaseTest
     */
     public function testMakeForDuplicatedLetter($letter)
     {
-        $cell_1 = new Cell(1);
-        $cell_2 = new Cell(2);
+        $cell_1 = $this->cells->at(1);
+        $cell_2 = $this->cells->at(2);
         $cell_2->setCharacter('O');
-        $cell_4 = new Cell(4);
+        $cell_4 = $this->cells->at(4);
         $cell_4->setCharacter('E');
         $word = new Word([$cell_1, $cell_2, $cell_1, $cell_4]);
-        $word_pattern = new WordPattern;
+        $word_pattern = new WordPattern($this->cells);
         $actual = $word_pattern->make($letter, $cell_1, $word); 
-        $expected = sprintf('^(%s)(O)(%s)(E)$', $letter, $letter);
+        $expected = sprintf('^%sO%sE$', $letter, $letter);
         $this->assertSame($expected, $actual);
     }
 
@@ -63,7 +70,7 @@ class WordPatternTest extends BaseTest
 
     protected function makeCell($index, $letter)
     {
-        $cell = new Cell($index);
+        $cell = $this->cells->at($index);
         if (!is_null($letter)){
             $cell->setCharacter($letter);
        }
--- lib/Codewords/Solver/WordPattern.php
+++ lib/Codewords/Solver/WordPattern.php
@@ -1,6 +1,7 @@
 <?php namespace Codewords\Solver;
 
 use Codewords\Board\Cell;
+use Codewords\Board\CellCollection;
 use Codewords\Board\Word;
 
 /**
@@ -8,13 +9,13 @@ use Codewords\Board\Word;
 */
 class WordPattern
 {
-    public function __construct()
+    public function __construct(CellCollection $cells)
     {
+        $this->cells = $cells;
     }
     
     /**
     * return the pattern to use to match a Word using $letter in place of $cell
-    * @todo add support for back references to letters that appear multiple times
     * @todo restrict wild cards to chars that have still to be solved, eg replace . with [^A|B|C] if A, B and C have been solved
     * @return string
     */
@@ -22,28 +23,38 @@ class WordPattern
     {
         $pattern = '';
         $current = [];
-        $solved_index = 1;
+        $wild_card_index = 1;
+        $solved = $this->cells->getSolved();
+        $chars = array_map(function($cell){ return $cell->getCharacter();}, $solved);
+        if (!in_array($letter, $chars)){
+            $chars [] = $letter;
+        }
+        sort($chars);
+        // wild card = [^SOLVED|..]
+        $wild_card = '[^' . implode('|',$chars) . ']';
         // iterate over Word's cells
+        // wrap each wild card in pattern in () so back referencing is easier
         foreach($word as $index => $other_cell){
             if ($other_cell->matches($cell)){
-                $pattern .= '(' . $letter . ')';
-                $solved_index++;
+                $pattern .= $letter;
             } else {
                 if ($char = $other_cell->getCharacter()){
                     // check for back references
+                    $pattern .= $char;
+                } else {
                     if (isset($current[$other_cell->getNumber()])){
                         // use \$num
                         $pattern .= '\\' . $current[$other_cell->getNumber()];
                     } else {
-                        $current[$other_cell->getNumber()] = $solved_index;
-                        $solved_index++;
-                        $pattern .= '('.$char.')';
+                        $current[$other_cell->getNumber()] = $wild_card_index;
+                        $pattern .= '(' . $wild_card . ')';
+                        $wild_card_index++;
                     }
-                } else {
-                    $pattern .= '.';
                 }
             }
         }
         return '^'.$pattern.'$';
     }
 }
+
+
