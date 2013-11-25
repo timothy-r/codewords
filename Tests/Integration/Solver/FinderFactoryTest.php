45114d35b2e5c7728b3c15721a395957c3266d8b
Tim Rodger <tim.rodger@gmail.com>
2013-11-25 11:55:29 +0000

    Moves FinderFactory test case

--- Tests/Integration/Solver/FinderFactoryTest.php
+++ Tests/Integration/Solver/FinderFactoryTest.php
@@ -1,6 +1,6 @@
 <?php
-require_once(__DIR__ . '/IntegrationTest.php');
-require_once(__DIR__. '/FixtureTrait.php');
+require_once(__DIR__ . '/../IntegrationTest.php');
+require_once(__DIR__. '/../FixtureTrait.php');
 
 use Codewords\Solver\FinderFactory;
 
