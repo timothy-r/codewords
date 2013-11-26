aeba1c7125bf73f85df99b87cb97fcdc35e15e19
Tim Rodger <tim.rodger@gmail.com>
2013-11-26 15:17:43 +0000

    Adds service config

---
+++ run.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once(__DIR__. '/vendor/autoload.php');
+
+use Codewords\Game;
+use Codewords\StrategyB;
+use Codewords\Dictionary\SortedDictionary;
+
+function usage()
+{
+    print "run.php \$data_file\n";
+    exit;
+}
+
+/**
+* run the application using $arg 1 as the data
+*/
+if (count($argv) < 2){
+    usage();
+}
+
+$file = $argv[1];
+if (!is_readable($file)){
+    usage();
+}
+
+$data = file_get_contents($file);
+
+$dict = new SortedDictionary(__DIR__."/config/dict-2");
+$game = new Game($data, $dict);
+$strategy = new StrategyB;
+
+$strategy->solve($game);
