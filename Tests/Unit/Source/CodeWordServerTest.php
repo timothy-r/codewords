eb990a665645bdfbd90871e8a3e0f19e1bc70093
Tim Rodger <tim.rodger@gmail.com>
2013-11-28 17:10:12 +0000

    Adds CodeWordServer fixture

--- Tests/Unit/Source/CodeWordServerTest.php
+++ Tests/Unit/Source/CodeWordServerTest.php
@@ -1,26 +1,30 @@
 <?php
 require_once(__DIR__ . '/../BaseTest.php');
 
+use Codewords\Source\CodeWordServer;
 use Codewords\Test\FixtureTrait;
 
-class CodeWordServer extends BaseTest
+class CodeWordServerTest extends BaseTest
 {
     use FixtureTrait;
 
     public function getFixtures()
     {
         return [
-            ['CodeWordServer.aspx'],
+            ['CodeWordServer.aspx', 'CodeWordServer.csv'],
         ];
     }
     
     /**
     * @dataProvider getFixtures
     */
-    public function testReadData($fixture)
+    public function testReadData($fixture, $expected)
     {
         $data = $this->getFixture($fixture);
-
+        $reader = new CodeWordServer;
+        $result = $reader->read($data);
+        $expected = $this->getFixture($expected);
+        $this->assertSame($expected, $result); 
     }
 
 }
