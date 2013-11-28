685a7e8de8cac7bfc4e28b6a9ff6e7d182afc266
Tim Rodger <tim.rodger@gmail.com>
2013-11-28 18:28:16 +0000

    Adds test for invalid data

--- Tests/Integration/Solver/StrategyBTest.php
+++ Tests/Integration/Solver/StrategyBTest.php
@@ -9,14 +9,27 @@ use Codewords\Board\Cell;
 use Codewords\Solver\FinderFactory;
 use Codewords\Solver\CellOptions;
 
+/**
+* @group slow
+* @group integration
+*/
 class StrategyBIntegrationTest extends IntegrationTest
 {
     use IntegrationFixtureTrait;
     use FixtureTrait;
 
-    public function testSolve()
+    public function getFixtures()
     {
-        $fixture = 'data-4.csv';
+        return [
+            ['data-4.csv', 'abroletkfqiyvpgdxnzjshwucm'],
+        ];
+    }
+
+    /**
+    * @dataProvider getFixtures
+    */
+    public function testSolve($fixture, $soln)
+    {
         $this->givenASortedDictionary();
         $this->givenAGame($fixture);
         $finder_factory = new FinderFactory($this->game);
@@ -25,36 +38,31 @@ class StrategyBIntegrationTest extends IntegrationTest
 
         $result = $strategy->solve($this->game);
         $cells = $this->game->getCells();
-        $this->assertCellCharacter($cells->at(1), 'a');
-        $this->assertCellCharacter($cells->at(2), 'b');
-        $this->assertCellCharacter($cells->at(3), 'r');
-        $this->assertCellCharacter($cells->at(4), 'o');
-        $this->assertCellCharacter($cells->at(5), 'l');
-        $this->assertCellCharacter($cells->at(6), 'e');
-        $this->assertCellCharacter($cells->at(7), 't');
-        $this->assertCellCharacter($cells->at(8), 'k');
-        $this->assertCellCharacter($cells->at(9), 'f');
-        $this->assertCellCharacter($cells->at(10), 'q');
-        $this->assertCellCharacter($cells->at(11), 'i');
-        $this->assertCellCharacter($cells->at(12), 'y');
-        $this->assertCellCharacter($cells->at(13), 'v');
-        $this->assertCellCharacter($cells->at(14), 'p');
-        $this->assertCellCharacter($cells->at(15), 'g');
-        $this->assertCellCharacter($cells->at(16), 'd');
-        $this->assertCellCharacter($cells->at(17), 'x');
-        $this->assertCellCharacter($cells->at(18), 'n');
-        $this->assertCellCharacter($cells->at(19), 'z');
-        $this->assertCellCharacter($cells->at(20), 'j');
-        $this->assertCellCharacter($cells->at(21), 's');
-        $this->assertCellCharacter($cells->at(22), 'h');
-        $this->assertCellCharacter($cells->at(23), 'w');
-        $this->assertCellCharacter($cells->at(24), 'u');
-        $this->assertCellCharacter($cells->at(25), 'c');
-        $this->assertCellCharacter($cells->at(26), 'm');
+        for($i = 1; $i < 26; $i++){
+            $letter = substr($soln, $i-1, 1);
+            $this->assertCellCharacter($cells->at($i), $letter);
+        }
     }
 
     protected function assertCellCharacter(Cell $cell, $character)
     {
         $this->assertSame($character, $cell->getCharacter());
+    }
+
+    public function getInvalidFixtures()
+    {
+        return [
+            ['CodeWordServer.csv', 'abroletkfqiyvpgdxnzjshwucm'],
+        ];
+    }
+
+    /**
+    * @dataProvider getInvalidFixtures
+    * @expectedException Codewords\Error\InvalidBoardData
+    */
+    public function testSolveDetectsInvalidBoards($fixture)
+    {
+        $this->givenASortedDictionary();
+        $this->givenAGame($fixture);
     }
 }
