6895a8e89fe36073cce36ae407ff7defbbf18e1a
Tim Rodger <tim.rodger@gmail.com>
2013-11-28 18:09:04 +0000

    Completes implementation of Code2 source reader

--- lib/Codewords/Source/Code2.php
+++ lib/Codewords/Source/Code2.php
@@ -6,14 +6,49 @@ class Code2 implements ISource
 {
     public function read($data)
     {
-        // strip out non-xml chars and read xml into a DomDocument 
-        $xml = $this->convertToXml($data);
-        $doc = new \DomDocument();
-        $doc->loadXml($xml);
-        print $doc->saveXml();
+        $xml = $this->convertStringToXml($data);
+        $data = $this->convertXmlToArray($xml);
+        $csv = $this->convertArrayToCsv($data);
+        return $csv;
+    }
+    
+    protected function convertArrayToCsv($data)
+    {
+        $csv = '';
+        foreach($data as $row){
+            $csv .= implode(',',$row). "\n";
+        }
+        return $csv;
     }
 
-    protected function convertToXml($data)
+    protected function convertXmlToArray($xml)
+    {
+        $data = [];
+
+        $doc = new \DomDocument();
+        $doc->loadXml($xml);
+        $cells = $doc->getElementsByTagName('cell');
+        foreach($cells as $cell){
+            $x = (int) $cell->getAttribute('x');
+            $y = (int) $cell->getAttribute('y');
+            if ($cell->hasAttribute('number')){
+                $value = $cell->getAttribute('number');
+            } else {
+                $value = 'x';
+            }
+            // x is the horizontal y is the vertical
+            if (!isset($data[$y])){
+                $data[$y] = [];
+            }
+            $data[$y][$x] = $value;
+        }
+        return $data;
+    }
+
+    /** 
+    * strip out non-xml chars and read xml into a DomDocument 
+    */
+    protected function convertStringToXml($data)
     {
         // remove all chars up to the first '<'
         $pos = strpos($data, '<');
