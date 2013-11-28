c33c4a383e1386d1efd1ce3866b11534e19e1374
Tim Rodger <tim.rodger@gmail.com>
2013-11-28 00:55:47 +0000

    Updates runner script to use di container

--- run.php
+++ run.php
@@ -1,16 +1,24 @@
 <?php
 require_once(__DIR__. '/vendor/autoload.php');
 
-use Codewords\Game;
-use Codewords\Solver\StrategyB;
-use Codewords\Dictionary\SortedDictionary;
-use Codewords\Board\HtmlTableBoardRenderer;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 function usage()
 {
     print "run.php \$data_file\n";
     exit;
 }
+
+function getDiContainer($file)
+{
+    $container = new ContainerBuilder();
+    $loader = new XmlFileLoader($container, new FileLocator(__DIR__));
+    $loader->load($file);
+    return $container; 
+}
+
 
 /**
 * run the application using $arg 1 as the data
@@ -26,13 +34,15 @@ if (!is_readable($file)){
 
 $data = file_get_contents($file);
 
-$dict = new SortedDictionary(__DIR__."/config/dict-3");
-$game = new Game($data, $dict);
+$container = getDiContainer(__DIR__ . '/config/services.xml');
+
+$game = $container->get('game');
+$game->load($data);
+
+$renderer = $container->get('renderer');
 //print $renderer->render($game->getBoard());
 
-$strategy = new StrategyB;
-
+$strategy = $container->get('strategy_b');
 $strategy->solve($game);
 
-$renderer = new HtmlTableBoardRenderer;
 print $renderer->render($game->getBoard());
