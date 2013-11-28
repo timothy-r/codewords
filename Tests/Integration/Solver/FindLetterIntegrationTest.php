166b0a120794fe652b5d576459c837702e846d7f
Tim Rodger <tim.rodger@gmail.com>
2013-11-28 00:03:11 +0000

    Updates test expectations for new dictionary file

--- Tests/Integration/Solver/FindLetterIntegrationTest.php
+++ Tests/Integration/Solver/FindLetterIntegrationTest.php
@@ -13,15 +13,15 @@ class FindLetterIntegrationTest extends IntegrationTest
     {
         return [
             ['data-1.csv', 'i', [1,2,3,6,7,9,10,11,15,16,17,19,21,23], []], 
-            ['data-3.csv', 'i', [4,5,7,9,12,13,16,26], [17 => 'n', 6 => 'p']],
+            ['data-3.csv', 'i', [3,4,5,7,9,12,13,26], [17 => 'n', 6 => 'p']],
             ['data-1.csv', 'q', [3,16], []], 
             ['data-3.csv', 'q', [12,25], [17 => 'n', 6 => 'p', 3 => 'i']],
-            ['data-1.csv', 'u', [1,3,10,15,16,19,23], []],
-            ['data-3.csv', 'u', [4,7,12,13,19,26], [17 => 'n', 6 => 'p', 3 => 'i']],
-            ['data-3.csv', 'e', [7,12,13,26], [17 => 'n', 6 => 'p', 3 => 'i']],
+            ['data-1.csv', 'u', [1,10,15,16,19], []],
+            ['data-3.csv', 'u', [4,12,19,26], [17 => 'n', 6 => 'p', 3 => 'i']],
+            ['data-3.csv', 'e', [12,13,15,26], [17 => 'n', 6 => 'p', 3 => 'i']],
         ];
     }
-    
+
     /**
     * @dataProvider getValidGameData
     */
@@ -41,7 +41,7 @@ class FindLetterIntegrationTest extends IntegrationTest
 
         $numbers = array_map(function($cell){ return $cell->getNumber();}, $results);
         // debug 
-        //var_dump(implode(',', $numbers));
+        var_dump(implode(',', $numbers));
 
         $this->assertTrue(is_array($results), "Expected solve() to return an array");
         $this->assertSame(count($expected), count($results), "Expected solve() to return ".implode(',', $expected)." not " . implode(',',$numbers));
